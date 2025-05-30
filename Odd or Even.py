075dbcf3850847ad8a0db24e63d6a685c5135c12
G. GUNA PRIYAN <gunapriyan10@gmail.com>
2025-05-30 17:13:58 +0530

    Add Even or Odd Checker program

---
+++ Odd or Even.py
@@ -0,0 +1,10 @@
+# even_or_odd.py
+
+# Ask the user for a number
+number = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
